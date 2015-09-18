6a5d1b0f972e97331e47fe3a7acc31cc9ea2c95f
Phillip Nguyen <phillip.d.nguyen23@gmail.com>
2015-09-17 20:45:34 -0700

    added user guess

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -11,6 +11,27 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+
+    var randomNum = Math.floor((100*Math.random())+1);
+
+    $(".new").on("click", function() {
+        randomNum = Math.floor((100*Math.random())+1);;
+        $(".guessBox").empty();
+        $("#count").html(0);
+        $("#feedback").html("Make your Guess!");
+    })
+
+
+    $(".button").on("click", function() {
+        if ($("#userGuess").val() === randomNum) {
+              $("#feedback").empty().append("That is correct");
+         } else {
+              $("#feedback").empty().append("That is wrong");
+        }
+
+    })
+
 
 });
 
